625b318653270da79e59e4034ad1e8369920e0ea
amparo <amparosonia59@gmail.com>
2024-03-20 22:25:58 -0600

    avance mvc productsController, productsService, ProductModel

--- huellitas/src/main/java/com/huellitas/huellitas/controller/productosController.java
+++ huellitas/src/main/java/com/huellitas/huellitas/controller/productosController.java
@@ -1,6 +1,62 @@
 package com.huellitas.huellitas.controller;
 
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huellitas.huellitas.model.ProductosModel;
+import com.huellitas.huellitas.service.ProductService;
+
+@RestController
+@RequestMapping("/api/products/")
 public class productosController {
+	
+	private final ProductService productService;// variable service
+
+	@Autowired
+public productosController(ProductService productService) {
+	this.productService= productService;//instancia de la clase. 
+}
+	@GetMapping
+	public ArrayList<ProductosModel> getProduct(){
+		return productService.getAllProducts();
+		
+	}
+	//get para un solo producto.
+	@GetMapping (path = "{prodId}")
+	public ProductosModel getProduct(@PathVariable("prodId")int prodId) {
+		return productService.getProduct(prodId);
+	}
+	
+	@PostMapping
+	public ProductosModel addProductModel(@RequestBody ProductosModel productosModel) {
+		return productService.addProductModel(productosModel);
+	}
+	
+	//Dell
+	@DeleteMapping(path = "{prodId}")
+	public ProductosModel deleteProduct(@PathVariable("prodId")int prodId) {
+		return productService.deleteProduct(prodId);
+	}
+	@PutMapping(path = "{prodId}")
+	public ProductosModel updateProductModel(@PathVariable("{prodId}" )int prodId,
+			@RequestBody ProductosModel productosModel) {
+		
+		return productService.updateProductModel(prodId, 
+				productosModel.getImagen(),
+				productosModel.getNombre_producto(),
+				productosModel.getDescripcion(),
+				Double.valueOf(productosModel.getPrecio()));
+		
+	}
+	
 	
 }
--- huellitas/src/main/java/com/huellitas/huellitas/service/ProductService.java
+++ huellitas/src/main/java/com/huellitas/huellitas/service/ProductService.java
@@ -1,5 +1,84 @@
 package com.huellitas.huellitas.service;
 
-public class ProductService {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
+
+import org.springframework.stereotype.Service;
+
+import com.huellitas.huellitas.model.ProductosModel;
+
+@Service
+public class ProductService {
+	public final ArrayList<ProductosModel> list=  new ArrayList<ProductosModel>();
+	
+	public ProductService ()
+	{
+	
+		list.add(new ProductosModel( "RopaMinie.jpg","RopaMinie" ,"Ropa para mascota, disney minnie mouse, camisa textil, rosa y grande" , 145.80));
+		list.add(new ProductosModel( "dssd.jpg","Ropa" ,"Ropa para mascota, disney minnie mouse" , 125.80));
+		list.add(new ProductosModel( "Peluche dinosaurio.jpg","aghdjdhd" ,"Ropa para mascota, disney minnie mouse, camisa textil, rosa y grande" , 15.80));
+	}
+
+	public ArrayList<ProductosModel> getAllProducts() {
+		// TODO Auto-generated method stub
+		return list;//devuelve la lista de productos
+	}
+
+	public ProductosModel getProduct(int prodId) {
+		// TODO Auto-generated method stub
+		ProductosModel productoTemporal= null;
+		for(ProductosModel product:list) {
+			if(product.getId_producto()==prodId) {
+				productoTemporal = product;
+				break;
+						
+			}
+			
+		}
+		return productoTemporal;
+	}
+	//Si tu no tienes constructor vacio jamas funcionara, y si escribes Productsmolds menos lo hara
+	public ProductosModel addProductModel(ProductosModel productosModel) {
+		ProductosModel productoTemporal =null;
+		if (list.add(productosModel)) {
+			productoTemporal=productosModel;
+		}
+		return null;
+	}
+
+	public ProductosModel deleteProduct(int prodId) {
+		ProductosModel productoTemporal=null;
+		for(ProductosModel product:list) {
+			if(product.getId_producto()==prodId) {
+				productoTemporal=product;
+				list.remove(product);//borra el producto de la lista;
+				break;
+			}
+		}
+		return null;
+	}
+
+	public ProductosModel updateProductModel(int prodId, String imagen, String nombre_producto, String descripcion,
+			Double precio) {
+		// TODO Auto-generated method stub
+		ProductosModel productoTemporal=null;
+			for(ProductosModel product:list) {
+				if(product.getId_producto()==prodId) {
+					if(imagen.length()!=0) product.setImagen(imagen);
+					if(nombre_producto.length()!=0) product.setDescripcion(descripcion);
+					if(descripcion.length()!=0)product.setDescripcion(descripcion);
+					if(precio.doubleValue()>0) product.setPrecio(precio);
+					productoTemporal = product;
+					break;
+				}
+			}
+		return productoTemporal;
+	}
+
+
+	
+	
 }
